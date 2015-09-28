f553aff62fcc8a0c6f375a66aa03884caa91598c
schooolman <j.schoolmee@gmail.com>
2015-09-27 22:55:11 -0500

    SUPER Basic carousel. Needs styling

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ var express = require('express');
 
 var app = express();
 
+var studentInfo = require('./public/assets/data/students.json')
 
 var server = app.listen(3000, function(){
     var port = server.address().port;
@@ -12,10 +13,15 @@ var server = app.listen(3000, function(){
 
 });
 
-app.use(express.static(__dirname, + '/public'));
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(request, response){
     console.log('Hit empty route');
     response.sendFile(__dirname + '/public/views/index.html');
 });
 
+app.get('/studentInfo', function(request,response){
+    console.log("hit student info");
+    response.send(studentInfo);
+})
